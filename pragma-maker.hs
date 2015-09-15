ac2ff3f8cd86352e14ed0a424ca2fc11e4cffb13
MichaeGon <lodnix@gmail.com>
2015-09-16 00:44:31 +0900

    delete tab ver

---
+++ pragma-maker.hs
@@ -0,0 +1,48 @@
+{-# OPTIONS_GHC -Wall -Werror #-}
+import Control.Exception
+import Control.Monad
+import Data.List
+import System.Directory
+import System.Environment
+import System.IO
+
+main :: IO ()
+main = getArgs >>= write
+    where
+        write (dist : lib : inc : _) = readFile dist >>= writeIncludes dist inc >> writePragmas dist lib
+        write _ = error "write"
+
+writePragmas :: FilePath -> FilePath -> IO ()
+writePragmas x = appendFile x . makeContents <=< getDirectoryContents
+
+writeIncludes :: FilePath -> FilePath -> String -> IO ()
+writeIncludes x y zs = bracketOnError (openTempFile "." "temp") finalize editTemp
+    where
+        finalize (tempName, tempHandle) = hClose tempHandle >> removeFile tempName
+        editTemp (tempName, tempHandle) = searchIncludes y
+                                        >>= hPutStr tempHandle . (++ zs) . unlines . map addinc
+                                        >> hClose tempHandle
+                                        >> removeFile x
+                                        >> renameFile tempName x
+        addinc s = "#include <llvm/" ++ s ++ ">"
+
+makeContents :: [FilePath] -> String
+makeContents = unlines . ("" :) . foldr ff []
+    where
+        ff x acc
+            | ".lib" `isSuffixOf` x = ("#pragma comment( lib, \"" ++ x ++ "\" )") : acc
+            | otherwise = acc
+
+searchIncludes :: FilePath -> IO [FilePath]
+searchIncludes x = getDirectoryContents x >>= searchDir x
+
+searchDir :: FilePath -> [FilePath] -> IO [FilePath]
+searchDir _ [] = return []
+searchDir x (z : zs)
+    | ".h" `isSuffixOf` z = (z :) <$> res
+    | '.' `elem` z = res
+    | otherwise = (++) <$> indir <*> res
+    where
+        x' = x ++ '/' : z
+        indir = map (\y -> z ++ '/' : y) <$> searchIncludes x'
+        res = searchDir x zs
